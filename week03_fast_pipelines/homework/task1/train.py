069d05af6918efbf751ca943ddb3813c5e183953
timka-byzov <timka.byzov@yandex.ru>
2025-02-16 21:57:41 +0000

    task1 refactor

--- week03_fast_pipelines/homework/task1/train.py
+++ week03_fast_pipelines/homework/task1/train.py
@@ -6,9 +6,8 @@ from unet import Unet
 
 from dataset import get_train_data
 
-
 import torch
-from enum import Enum
+
 
 
 class LossScaler:
@@ -16,15 +15,12 @@ class LossScaler:
     def __init__(self, scale=1):
         self.cur_scale = scale
 
-    # `params` is a list / generator of torch.Variable
     def has_overflow(self, params):
         return False
 
-    # `x` is a torch.Tensor
     def _has_inf_or_nan(x):
         return False
 
-    # `overflow` is boolean indicating whether we overflowed in gradient
     def update_scale(self, overflow):
         pass
 
@@ -51,26 +47,17 @@ class DynamicLossScaler:
         self.scale_factor = scale_factor
         self.scale_window = scale_window
 
-    # `params` is a list / generator of torch.Variable
     def has_overflow(self, params):
-#        return False
         for p in params:
             if p.grad is not None and DynamicLossScaler._has_inf_or_nan(p.grad.data):
                 return True
 
         return False
 
-    # `x` is a torch.Tensor
     def _has_inf_or_nan(x):
         try:
-            # Stopgap until upstream fixes sum() on HalfTensors
             cpu_sum = float(x.float().sum())
-            # cpu_sum = float(x.sum())
-            # print(cpu_sum)
         except RuntimeError as instance:
-            # We want to check if inst is actually an overflow exception.
-            # RuntimeError could come from a different error.
-            # If so, we still want the exception to propagate.
             if "value cannot be converted" not in instance.args[0]:
                 raise
             return True
@@ -79,16 +66,13 @@ class DynamicLossScaler:
                 return True
             return False
 
-    # `overflow` is boolean indicating whether we overflowed in gradient
     def update_scale(self, overflow):
         if overflow:
-            #self.cur_scale /= self.scale_factor
             self.cur_scale = max(self.cur_scale/self.scale_factor, 1)
             self.last_overflow_iter = self.cur_iter
         else:
             if (self.cur_iter - self.last_overflow_iter) % self.scale_window == 0:
                 self.cur_scale *= self.scale_factor
-#        self.cur_scale = 1
         self.cur_iter += 1
 
     @property
@@ -129,10 +113,9 @@ def train_epoch(
         scaled_loss.backward()
 
         params = model.parameters()
-        # Check for overflow
+
         has_overflow = loss_scaler.has_overflow(params)
         
-        # If no overflow, unscale grad and update as usual
         if not has_overflow:
             for param in  params:
                 param.grad.data.mul_(1. / loss_scaler.loss_scale)
@@ -140,8 +123,6 @@ def train_epoch(
 
         
         loss_scaler.update_scale(has_overflow)
-
-        # end of my code
 
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
